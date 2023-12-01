c6985838233bba6bac3763958a795cba6a127042
Joel Petracci <jpetracci@gmail.com>
2023-12-01 00:52:26 -0800

    small update to change to u32

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -54,16 +54,16 @@ fn part2(input: &str) -> String {
     let mut sum = 0;
 
     for l in lines {
-        let mut digits: Vec<String> = Vec::new();
+        let mut digits: Vec<u32> = Vec::new();
 
         // find digits
         for (i, c) in l.chars().enumerate() {
             if c.is_digit(10) {
-                digits.push(c.to_string());
+                digits.push(c.to_digit(10).unwrap_or(0));
             }
             for (idx, num) in numbers.iter().enumerate() {
                 if l[i..l.len()].starts_with(num) {
-                    digits.push((idx + 1).to_string());
+                    digits.push((idx + 1) as u32);
                 }
             }
         }
@@ -72,8 +72,8 @@ fn part2(input: &str) -> String {
         let number = if digits.len() > 0 {
             let s = format!(
                 "{}{}",
-                digits.first().unwrap_or(&"0".to_string()),
-                digits.last().unwrap_or(&"0".to_string())
+                digits.first().unwrap_or(&0),
+                digits.last().unwrap_or(&0)
             );
             let value: u32 = s.parse::<u32>().unwrap_or(0);
             value
