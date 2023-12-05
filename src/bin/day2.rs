b9aa8ef33356933fdb28e19ccd821523e71e96fd
Joel Petracci <jpetracci@gmail.com>
2023-12-04 16:25:55 -0800

    day 2 p2

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -6,16 +6,8 @@ fn main() {
     dbg!(output2);
 }
 
-#[derive(Debug)]
-struct Game {
-    game: u32,
-    number: u32,
-    color: String,
-}
-
 fn part1(input: &str) -> String {
     let lines = input.split("\n").collect::<Vec<&str>>();
-    let mut game_draws: Vec<Game> = Vec::new();
 
     let max_red = 12;
     let max_green = 13;
@@ -59,7 +51,43 @@ fn part1(input: &str) -> String {
 }
 
 fn part2(input: &str) -> String {
-    "todo".to_string()
+    let lines = input.split("\n").collect::<Vec<&str>>();
+
+    let mut game_sum = 0;
+
+    for line in lines {
+        let mut max_red = 0;
+        let mut max_green = 0;
+        let mut max_blue = 0;
+
+        let values = line.split(":").collect::<Vec<&str>>();
+
+        if values.len() == 2 {
+            let vals = values[1].split_whitespace().collect::<Vec<&str>>();
+            for (i, v) in vals.iter().enumerate() {
+                if v.find("blue") != None {
+                    let value = vals[i - 1].parse::<u32>().unwrap_or(0);
+                    if value > max_blue {
+                        max_blue = value;
+                    }
+                }
+                if v.find("red") != None {
+                    let value = vals[i - 1].parse::<u32>().unwrap_or(0);
+                    if value > max_red {
+                        max_red = value;
+                    }
+                }
+                if v.find("green") != None {
+                    let value = vals[i - 1].parse::<u32>().unwrap_or(0);
+                    if value > max_green {
+                        max_green = value;
+                    }
+                }
+            }
+            game_sum += (max_red * max_green * max_blue);
+        }
+    }
+    game_sum.to_string()
 }
 
 #[cfg(test)]
@@ -81,14 +109,12 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
     #[test]
     fn part2_test() {
         let result = part2(
-            "two1nine
-eightwothree
-abcone2threexyz
-xtwone3four
-4nineeightseven2
-zoneight234
-7pqrstsixteen",
+            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
         );
-        assert_eq!(result, "281");
+        assert_eq!(result, "2286");
     }
 }
